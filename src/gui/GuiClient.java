4437fc840a3311ff6f65d6759274e64ee24fb46f
gudnims <gudnims@gmail.com>
2016-09-12 13:05:26 +0200

    Design af gui første version

---
+++ src/gui/GuiClient.java
@@ -0,0 +1,23 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by Gudni on 12-09-2016.
+ */
+public class GuiClient {
+    private JPanel mainPanel;
+    private JLabel userLabel;
+    private JLabel ipLabel;
+    private JLabel portLabel;
+    private JTextField userField;
+    private JTextField ipField;
+    private JTextField portField;
+    private JButton joinChatButton;
+    private JButton logOutButton;
+    private JTextArea writeMessage;
+    private JButton sendMessageButton;
+    private JTextArea showMessage;
+    private JTextArea textArea2;
+
+}
