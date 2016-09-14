ec159c8026e693954ee53477e65ed32634d45c4b
gudnims <gudnims@gmail.com>
2016-09-14 09:32:17 +0200

    Funktioner til Gui

--- src/gui/GuiClient.java
+++ src/gui/GuiClient.java
@@ -1,6 +1,11 @@
 package gui;
 
 import javax.swing.*;
+import control.Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * Created by Gudni on 12-09-2016.
@@ -18,6 +23,49 @@ public class GuiClient {
     private JTextArea writeMessage;
     private JButton sendMessageButton;
     private JTextArea showMessage;
-    private JTextArea textArea2;
+    private JTextArea showUsersArea;
+    private Controller control;
 
+
+    public GuiClient(Controller control) {
+        this.control = control;
+        sendMessageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (writeMessage.getText().length() <= 250) {
+                    control.sendChatLine(writeMessage.getText());
+                    writeMessage.setText("");
+                } else {
+                    JOptionPane.showMessageDialog(null, "You're message is to long, it may only contain 256 characters");
+                }
+            }
+        });
+
+        joinChatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    control.connect(ipField.getText(), Integer.parseInt(portField.getText()), userField.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        logOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    control.logout();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+
+    }
 }
+
+
+
