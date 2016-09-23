294ac6bae2065b031ab111dd872596254358d9a9
Jakob Ferdinandsen <jakob@jhaaf.dk>
2016-09-23 13:46:38 +0200

    Added client and reader

---
+++ src/client/Reader.java
@@ -0,0 +1,43 @@
+package client;
+
+import java.io.BufferedReader;
+
+/**
+ * Created by jakob on 07-09-16.
+ */
+public class Reader implements Runnable{
+    private Client client;
+    private BufferedReader fromServer;
+
+    public Reader(Client client) {
+        this.client = client;
+        this.fromServer = client.getFromServer();
+    }
+
+    @Override
+    public void run() {
+        String message;
+
+        try{
+            while ((message = fromServer.readLine()) != null){
+                if (message.startsWith("J_ERR")){
+                    client.showError("Username is already taken");
+                    client.closeConn();
+                }
+                if (message.startsWith("DATA ")){
+                    client.receiveMessage(message.substring(5));
+                }
+                if (message.startsWith("LIST ")){
+                    String[] result = message.substring(5).split(" ");
+                    client.updateListedUsers(result);
+                }
+                if (message.startsWith("J_OK")){
+                    client.joinOK();
+                }
+
+            }
+        }catch (Exception ex){
+            client.showError("Encountered an error while reading from socket");
+        }
+    }
+}
